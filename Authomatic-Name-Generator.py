d64289d54a69967cf826ffad5b9ff95f406147af
Risky Lucky Romadhon <riskyluckyromadhon@gmail.com>
2025-01-04 11:03:27 +0700

    Menambahkan file Authomatic-Name-Generator.py

---
+++ Authomatic-Name-Generator.py
@@ -0,0 +1,7 @@
+from calendar import month
+
+print ("Welcome to the Authomatic Name Generator")
+unique = input("Choose the unique name you want!\n")
+month = input("Which month did you born?\n")
+family = input("Which name of your family?\n")
+print("Your Authomatic Name could be: " + unique + " "+ month +" " + family)
